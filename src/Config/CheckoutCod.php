9ca1f3695fb5c36a55e748af2a5202dbea093a19
Steve <34465153+xxl4@users.noreply.github.com>
2024-11-06 16:23:24 +0800

    fix 1.0.4 to 1.0.3

--- src/Config/CheckoutCod.php
+++ src/Config/CheckoutCod.php
@@ -17,12 +17,12 @@ return [
      * 
      * The version of the package
      */
-    'version' => '1.0.4',
+    'version' => '1.0.3',
     /**
      * 
      * The version number of the package
      */
-    'versionNum' => '104',
+    'versionNum' => '103',
 
     /**
      *
