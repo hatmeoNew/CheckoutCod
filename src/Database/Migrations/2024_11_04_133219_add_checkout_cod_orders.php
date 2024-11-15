e706d7d472c6e4824dba6eb085f38a23ae93dbab
Steve <34465153+xxl4@users.noreply.github.com>
2024-11-15 16:25:50 +0800

    fix

--- src/Database/Migrations/2024_11_04_133219_add_checkout_cod_orders.php
+++ src/Database/Migrations/2024_11_04_133219_add_checkout_cod_orders.php
@@ -13,13 +13,19 @@ return new class extends Migration
     {
         //
         Schema::create("order_cods", function (Blueprint $table) {
-            $table->bigIncrements("id");
-            $table->unsignedBigInteger("order_id")->comment("Order ID");
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('order_id')->unsigned()->nullable()->comment("Order ID");
             // add create and update datetime
             $table->string("ip_address")->nullable()->comment("IP Address");
             $table->string("ip_country")->nullable()->comment("IP Country");
             $table->timestamps();
             //$table->foreign("order_id")->references("id")->on("orders");
+            //$table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+
+        Schema::table("order_cods", function (Blueprint $table) {
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
         });
     }
 
