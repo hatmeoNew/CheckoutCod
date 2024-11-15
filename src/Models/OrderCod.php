6edef3f581d0b2f301cbb1ecc05aac01efe76e09
Steve <34465153+xxl4@users.noreply.github.com>
2024-11-15 16:47:31 +0800

    fix the db table name

--- src/Models/OrderCod.php
+++ src/Models/OrderCod.php
@@ -8,11 +8,12 @@ use NexaMerchant\CheckoutCod\Contracts\OrderCod as OrderCodContract;
 
 class OrderCod extends Model implements OrderCodContract
 {
+    private $table = 'order_cods';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['ip_address', 'ip_country'];
+    protected $fillable = ['order_id','ip_address', 'ip_country'];
 
 }
