c581dd9239bc3a908b9fa8613ed0463ef451494f
Steve <34465153+xxl4@users.noreply.github.com>
2024-11-25 11:07:48 +0800

    fix checkcods

--- src/Contracts/OrderCod.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace NexaMerchant\CheckoutCod\Contracts;
-
-interface OrderCod
-{
-}
--- src/Models/OrderCods.php
+++ src/Models/OrderCods.php
@@ -4,9 +4,9 @@ namespace NexaMerchant\CheckoutCod\Models;
 
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Model;
-use NexaMerchant\CheckoutCod\Contracts\OrderCod as OrderCodContract;
+use NexaMerchant\CheckoutCod\Contracts\OrderCods as OrderCodsContract;
 
-class OrderCods extends Model implements OrderCodContract
+class OrderCods extends Model implements OrderCodsContract
 {
     /**
      * The attributes that are mass assignable.
