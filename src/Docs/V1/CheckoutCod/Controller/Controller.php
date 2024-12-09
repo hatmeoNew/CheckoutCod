f47dbbb60c6d72784329de489bbba048cec9945c
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-09 10:53:44 +0800

    add docs

---
+++ src/Docs/V1/CheckoutCod/Controller/Controller.php
@@ -0,0 +1,25 @@
+<?php
+namespace NexaMerchant\CheckoutCod\Docs\V1\CheckoutCod\Controller;
+
+/**
+ * @OA\Info(
+ *      version="1.1.0",
+ *      title="NexaMerchant CheckoutCod Rest API Documentation",
+ *      description="NexaMerchant CheckoutCod Rest API Documentation",
+ *
+ *      @OA\Contact(
+ *          email="nice.lizhi@gmail.com"
+ *    )
+ * )
+ * @OA\Server(
+ *    url="http://localhost:8000/api/v1",
+ *   description="NexaMerchant CheckoutCod End Rest API"
+ * )
+ * @OA\Tag(
+ *  name="CheckoutCod",
+ * description="API Endpoints of CheckoutCod"
+ * )
+ */
+class Controller
+{
+}
